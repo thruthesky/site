9389dbaf5c502cc25966f76f94097a6aed60dbee
lancelynyrd <pinedaclp@gmail.com>
2018-04-18 13:55:45 +0800

    adding setting page

---
+++ src/app/pages/settings/settings.page.ts
@@ -0,0 +1,54 @@
+import {Component} from '@angular/core';
+import {AppService} from '../../providers/app.service';
+
+
+@Component({
+    selector: 'settings-page',
+    templateUrl: 'settings.page.html',
+    styleUrls: ['settings.page.scss'],
+})
+export class SettingsPage {
+
+    tz = {};
+
+    userTZ = 0;
+
+    constructor(public a: AppService) {
+
+        a.lms.timezones().subscribe(re => {
+            // console.log( re);
+            this.tz = re;
+        });
+
+        if (a.user.isLogin) {
+            a.lms.timezone().subscribe(re => {
+                // console.log('timezone', re);
+                this.userTZ = re['offset'];
+            });
+        }
+    }
+
+    keysTimezone() {
+        return Object.keys(this.tz).sort((a: any, b: any) => a - b);
+    }
+
+    format(offset) {
+        if (offset > 0) {
+            return '+' + offset;
+        } else {
+            return offset;
+        }
+    }
+
+
+    onClickTimezone(offset) {
+        // console.log(offset);
+        this.a.lms.timezone_set(offset).subscribe(re => {
+            // console.log(re);
+            this.a.user.loadProfile().subscribe(() => {
+            });
+        }, e => this.a.alert(e));
+    }
+
+
+}
