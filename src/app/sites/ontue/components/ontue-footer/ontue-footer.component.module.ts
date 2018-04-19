a6122a952bdbdb13c5b5eb7e1aed4f5c7674a83f
JaeHo Song <thruthesky@gmail.com>
2018-04-20 01:09:00 +0800

    add. ontue my schedule. edit.

---
+++ src/app/sites/ontue/components/ontue-footer/ontue-footer.component.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { OntueFooterComponent } from './ontue-footer.component';
+import { MaterialModule } from '../../../../app.material.module';
+import { TranslatePipeModule } from '../../../../modules/firelibrary/pipes/translate/translate.pipe.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule,
+    MaterialModule,
+    TranslatePipeModule
+  ],
+  declarations: [
+    OntueFooterComponent
+  ],
+  exports: [
+    RouterModule,
+    OntueFooterComponent
+  ],
+  schemas: []
+})
+export class OntueFooterComponentModule { }
+
