c879878579a18885d22825b6ecee7cffce93f463
JaeHo Song <thruthesky@gmail.com>
2018-04-20 16:32:03 +0800

    adding sessions

---
+++ src/app/sites/katalkenglish/pages/katalkenglish-session-past/katalkenglish-session-past.page.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { TranslatePipeModule } from '../../../../modules/firelibrary/pipes/translate/translate.pipe.module';
+
+import { KatalkEnglishSessionPastPage } from './katalkenglish-session-past.page';
+import { SessionListComponentModule } from '../../../../components/session-list/session-list.component.module';
+
+const routes: Routes = [
+  { path: '', component: KatalkEnglishSessionPastPage }
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    TranslatePipeModule,
+    SessionListComponentModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+    KatalkEnglishSessionPastPage
+  ],
+  entryComponents: [
+    KatalkEnglishSessionPastPage
+  ],
+  bootstrap: [KatalkEnglishSessionPastPage],
+  schemas: []
+})
+export class KatalkEnglishSessionPastPageModule { }
+
