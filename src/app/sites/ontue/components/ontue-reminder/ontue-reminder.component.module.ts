f7dadfd07ac27f5a49a65dbb0ded542b33f2dac3
lancelynyrd <pinedaclp@gmail.com>
2018-05-23 02:49:58 +0800

    home reminder

---
+++ src/app/sites/ontue/components/ontue-reminder/ontue-reminder.component.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { MaterialModule } from '../../../../app.material.module';
+import { TranslatePipeModule } from '../../../../modules/firelibrary/pipes/translate/translate.pipe.module';
+import { OntueReminderComponent } from './ontue-footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule,
+    MaterialModule,
+    TranslatePipeModule
+  ],
+  declarations: [
+      OntueReminderComponent
+  ],
+  exports: [
+    RouterModule,
+    OntueReminderComponent
+  ],
+  schemas: []
+})
+export class OntueReminderComponentModule { }
+
--- src/app/sites/ontue/pages/ontue-home/ontue-home.page.ts
+++ src/app/sites/ontue/pages/ontue-home/ontue-home.page.ts
@@ -1,19 +1,34 @@
 import { Component } from '@angular/core';
 import { AppService } from '../../../../providers/app.service';
-import { FireService } from '../../../../modules/firelibrary/core';
 
 
 @Component({
-  selector: 'app-page-ontue-home',
-  templateUrl: 'ontue-home.page.html',
-  styleUrls: ['ontue-home.page.scss'],
+    selector: 'app-page-ontue-home',
+    templateUrl: 'ontue-home.page.html',
+    styleUrls: ['ontue-home.page.scss'],
 })
 export class OntueHomePage {
-  constructor(
-    public a: AppService
-  ) {
 
-  }
+
+    site_info = {
+        comment_from_student: [],
+        comment_from_teacher: [],
+        no_of_past: 0,
+        no_of_reservations: 0,
+        no_of_student: 0,
+        no_of_teacher: 0,
+        recent_graded_teachers: [],
+        recent_reservations: []
+    };
+
+
+    constructor(public a: AppService) {
+        this.a.lms.get_teacher_site_info().subscribe(res => {
+            this.site_info = res['site_info'];
+        }, () => {
+        });
+
+    }
 }
 
 
