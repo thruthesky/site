e6092b65a01a8b58533a520654f9140a832e8bf7
lancelynyrd <pinedaclp@gmail.com>
2018-06-02 15:07:49 +0800

    import

--- src/app/pages/message/message.page.module.ts
+++ src/app/pages/message/message.page.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 // import { TranslatePipeModule } from '../../modules/firelibrary/pipes/translate/translate.pipe.module';
 import { MessagePage } from './message.page';
 import { TranslatePipeModule } from '../../pipes/translate/translate.pipe.module';
+import { MaterialModule } from '../../app.material.module';
 
 
 const routes: Routes = [
