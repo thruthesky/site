7c1d3cf4e4753b3f5cc96f8b057f3412ebfec937
JaeHo Song <thruthesky@gmail.com>
2018-06-14 23:27:13 +0800

    update for withcenter

--- src/app/pages/session-refund-request/session-refund-request.page.ts
+++ src/app/pages/session-refund-request/session-refund-request.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AppService } from '../../providers/app.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
     templateUrl: 'session-refund-request.page.html',
     styleUrls: ['session-refund-request.page.scss'],
 })
-export class SessionRefundRequestPage implements OnInit {
+export class SessionRefundRequestPage implements OnInit, AfterViewInit {
 
     message = null;
     loadingRefundRequest = false;
@@ -21,8 +21,19 @@ export class SessionRefundRequestPage implements OnInit {
 
     ngOnInit() {
         this.active.queryParams.subscribe(params => {
-           this.idx = params.idx;
+            this.idx = params.idx;
         });
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            try {
+                const textarea: HTMLElement = document.getElementById('refund-request-textarea');
+                textarea.focus();
+            } catch {
+                console.error('Soft-error: Failed to put cursor on textarea.');
+            }
+        }, 300);
     }
 
 
@@ -35,16 +46,16 @@ export class SessionRefundRequestPage implements OnInit {
             this.a.lms.session_refund_request({
                 idx_reservation: this.idx,
                 refund_request_message: this.message
-            }).subscribe( () => {
+            }).subscribe(() => {
                 this.loadingRefundRequest = false;
                 this.a.open('session-past');
-                this.a.toast( this.a.t('REFUND REQUEST SUCCESS') );
+                this.a.toast(this.a.t('REFUND REQUEST SUCCESS'));
             }, e => {
                 this.a.toast(e);
                 this.loadingRefundRequest = false;
             });
         } else {
-            this.a.toast( this.a.t('MESSAGE EMPTY'));
+            this.a.toast(this.a.t('MESSAGE EMPTY'));
         }
     }
 }
