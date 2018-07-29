7fcc8e3dd81bf5515ba039d454f1545bbc94564f
JaeHo Song <thruthesky@gmail.com>
2018-07-29 16:59:36 +0800

    update for past session list

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
         storageBucket: '',
         messagingSenderId: '1068647439857'
     },
-    reloadTag: 201807192, // This can be manually set for prod.
+    reloadTag: 20180728, // This can be manually set for prod.
     disableFirebaseUserActivityLog: false,
     categories: {
         teacher_reminders: 2,
