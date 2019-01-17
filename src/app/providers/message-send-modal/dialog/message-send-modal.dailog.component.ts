5500099c2edff4dc293d1c4b7fa98fcc78f86dd3
lancelynyrd <pinedaclp@gmail.com>
2019-01-17 15:38:13 +0800

    update on message modal

--- src/app/providers/message-send-modal/dialog/message-send-modal.dailog.component.ts
+++ src/app/providers/message-send-modal/dialog/message-send-modal.dailog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AppService } from '../../app.service';
 
 @Component({
     selector: 'message-send-modal-dailog-component',
@@ -11,6 +12,7 @@ export class MessageSendModalDialogComponent {
     content = '';
     constructor(
         public dialogRef: MatDialogRef<MessageSendModalDialogComponent>,
+        public a: AppService,
         @Inject(MAT_DIALOG_DATA) public data: any
     ) {
 
